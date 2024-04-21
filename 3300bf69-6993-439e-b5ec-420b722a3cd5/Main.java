9458d727a5f5c5d075d6983c594ed5b7223ac6d6
YongBin Kim <yongbyn@naver.com>
2024-04-22 01:10:25 +0900

    refactor. 더미 파일 삭제

--- 3300bf69-6993-439e-b5ec-420b722a3cd5/Main.java
+++
@@ -1,7 +0,0 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Solution s = new Solution();
-        ObjectMapper mapper = new ObjectMapper();
-System.out.print(mapper.writeValueAsString(s.solution(mapper.readValue(args[0], int.class),mapper.readValue(args[1], int[].class))));}}
--- 3300bf69-6993-439e-b5ec-420b722a3cd5/Solution.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] solution(int N, int[] values) {
-        int[] answer = new int[values.length];
-        for (int i = 0; i < N; i++) {
-            answer[i] = values[i] * 2;
-        }
-        return answer;
-    }
-}
