956c22f2e569348c78f78b3029a448f0915428ab
hlmg <devhlmg@gmail.com>
2024-04-22 18:28:51 +0900

    fix: Validation 적용

--- src/main/java/clofi/codeython/problem/domain/request/CreateProblemRequest.java
+++ src/main/java/clofi/codeython/problem/domain/request/CreateProblemRequest.java
@@ -1,14 +1,17 @@
 package clofi.codeython.problem.domain.request;
 
 import clofi.codeython.problem.domain.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @AllArgsConstructor
@@ -21,18 +24,18 @@ public class CreateProblemRequest {
     @NotBlank(message = "문제설명은 공백일 수 없습니다.")
     private String content;
 
-    @NotBlank(message = "제한사항은 공백일 수 없습니다.")
+    @NotEmpty(message = "제한사항은 공백일 수 없습니다.")
     private List<String> limitFactors;
 
-    @NotBlank(message = "제한시간은 공백일 수 없습니다.")
-    @Size(min = 10, message = "")
-    private int limitTime;
+    @NotNull(message = "제한시간은 공백일 수 없습니다.")
+    @Min(value = 10, message = "제한시간은 최소 10분입니다.")
+    private Integer limitTime;
 
-    @NotBlank(message = "난이도는 공백일 수 없습니다.")
-    @Size(min = 1, max = 5, message = "난이도는 최소 1에서 최대 5입니다.")
-    private int difficulty;
+    @NotNull(message = "난이도는 공백일 수 없습니다.")
+    @Range(min = 1, max = 5, message = "난이도는 최소 1에서 최대 5입니다.")
+    private Integer difficulty;
 
-    @NotBlank(message = "입력값 타입은 공백일 수 없습니다.")
+    @NotEmpty(message = "입력값 타입은 공백일 수 없습니다.")
     private List<String> type;
 
     private List<BaseCodeRequest> baseCodes;
--- src/main/java/clofi/codeython/room/domain/request/CreateRoomRequest.java
+++ src/main/java/clofi/codeython/room/domain/request/CreateRoomRequest.java
@@ -3,6 +3,8 @@ package clofi.codeython.room.domain.request;
 import clofi.codeython.problem.domain.Problem;
 import clofi.codeython.room.domain.Room;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +17,11 @@ public class CreateRoomRequest {
     @NotBlank(message = "경기장 제목은 공백일 수 없습니다.")
     private String roomName;
 
-    @NotBlank(message = "문제 아이디는 공백일 수 없습니다.")
+    @NotNull(message = "문제 아이디는 공백일 수 없습니다.")
     private Long problemId;
 
-    @NotBlank(message = "인원 제한 수는 공백일 수 없습니다.")
+    @NotNull(message = "인원 제한 수는 공백일 수 없습니다.")
+    @Pattern(regexp = "^([246])$", message = "인원 제한 수는 2, 4, 6 중 하나여야 합니다.")
     private int limitMemberCnt;
 
     private boolean isSecret;
